815882cb0cc2b9e545a43ec5f75f2272781755c6
Mike Kuzin <mike.kuzin@showclix.com>
2011-06-28 13:57:29 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,125 @@
+<?
+/**
+ * Metamorphosis Initial Development
+ * Front end code, by Mike Kuzin
+ **/
+?>
+
+<!DOCTYPE html>
+<!--[if IE 7 ]>    <html class="ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title>
+            Metamorphosis Salon &amp; Spa » Home
+        </title>
+
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+
+        <meta name="author" content="humans.txt">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="assets/css/styles.css?v=1">
+
+        <script src="assets/js/libs/modernizr-1.7.min.js"></script>
+
+    </head>
+
+    <body>
+        <header id="main">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li class="selected">Services</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <a href="/" id="logo">Metamorphosis Salon &amp; Spa</a>
+        </header>
+        <section id="meta-inside">
+            <header>
+                <h1>Title of Section</h1>
+                <h2>Subtitle</h2>
+            </header>
+            <div class="content">
+                <p>This is content?! What?!</p>
+            </div>
+        </section>
+        <section id="meta-inside">
+            <header>
+                <h1>Title of Section</h1>
+                <h2>Subtitle</h2>
+            </header>
+            <div class="content">
+                <p>This is content?! What?!</p>
+            </div>
+        </section>
+        <footer>
+            <div id="footer-left">
+                <div id="footer-left-container">
+                    <p>
+                        5112 Butler Street<br />
+                        Lawrenceville, PA 15201
+                    </p>
+                    <a href="#" class="button">Get Directions</a>
+                    <hr/>
+                    <p>
+                        Open Monday &ndash; Saturday<br/>
+                        9 am &ndash; 5pm
+                    </p>
+                </div>
+            </div>
+            <div id="footer-right">
+                <div id="footer-right-container">
+                    <p id="contact-blurb">
+                        412.555.1212<br/>
+                        info@spapgh.com<br/>
+                        <a href="#" class="social">Twitter</a>
+                        <a href="#" class="social">Facebook</a>
+                        <a href="#" class="social">Flickr</a>
+                    </p>
+                    <a href="#" class="button">Schedule an Appointment</a>
+                    <p id="tweet-blurb">
+                        Maecenas faucibus mollis interdum. Morbi leo risus, porta ac
+                        consectetur ac, vestibulum at eros. Duis mollis, est non commodo.
+                    </p>
+                </div>
+                <div id="footer-right-footer">
+                    <a href="/" class="small-logo">Metamorphosis Salon &amp; Spa</a>
+                    <p>
+                        Content &copy; 2011 Metamorphosis. All Rights Reserved.<br />
+                        Design by <a href="http://www.kuzn.me">Kuzin</a>.
+                    </p>
+                </div>
+            </div>
+        </footer>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+        <script>
+            !window.jQuery && document.write(
+                unescape(
+                    '%3Cscript src="assets/js/libs/jquery-1.5.1.js"%3E%3C/script%3E'
+                )
+            )
+        </script>
+        <script src="assets/js/script.js"></script>
+        <!--[if lt IE7]>
+            <script src="assets/js/libs/dd_belated.js"></script>
+            <script>DD_belatedPNG.fix('img, .png_bg');</script>
+        <![endif]-->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+    		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    		s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
